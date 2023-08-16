f5671941f9b080768dd05c72b996acf1996967f7
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2023-08-16 14:40:27 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -180,7 +180,7 @@ consumer_download() {
     language_skuid_table_html="$(echo "$language_skuid_table_html" | head --bytes 10240)"
     echo_info "$language_skuid_table_html"
     # tr: Filter for only alphanumerics or "-" to prevent HTTP parameter injection
-    sku_id="$(echo "$language_skuid_table_html" | grep "$language" | sed 's/&quot;//g' | cut --delimiter ',' --fields 1  | cut --delimiter ':' --fields 2 | tr --complement --delete '[:alnum:]-' | head --bytes 16)"
+    sku_id="$(echo "$language_skuid_table_html" | grep "$language" | sed 's/&quot;//g' | cut -d ',' --fields 1  | cut -d ':' --fields 2 | tr --complement --delete '[:alnum:]-' | head --bytes 16)"
     [ "$VERBOSE" ] && echo "SKU ID: $sku_id" >&2
 
     # Get ISO download link
